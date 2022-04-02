a124dc8d6da21f52ab8090837e4323af08a9e141
Miguel Wister <mwister@hotmail.com>
2022-04-01 21:20:04 -0600

    commit

---
+++ conversion/pies.cpp
@@ -0,0 +1,27 @@
+<<<<<<< Updated upstream
+// 29 marzo 2022
+=======
+// 14:55
+>>>>>>> Stashed changes
+
+#include <iostream>
+#define pies 3.28
+
+using namespace std;
+
+
+int main() {
+  int i;
+  float c;
+
+  cout << "\\t" << "Metros" << "\\t" << "Pies";
+  cout << endl;
+
+  for (i=1; i<=40; i++) {
+    c = i * pies;
+    cout << "\\t" << i << "\\t" << c;
+    cout << endl;
+  }
+  return 0;
+}
+}
