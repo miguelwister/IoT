ec7aa9261254da8daf28a50bb9b67fb48d77c868
Miguel Wister <mwister@hotmail.com>
2022-04-01 22:46:48 -0600

    Merge branch 'main' of https://github.com/miguelwister/IoT into main

---
+++ nuevo/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main () {
+
+printf ("Hello World Magivo Disney\n");
+return 0;
+}
--- programasC/main.c
+++ programasC/main.c
@@ -2,6 +2,10 @@
 
 int main () {
 
+<<<<<<< HEAD
 printf ("Hello World Magico Disneyworld\n");
+=======
+printf ("Hello World\n");
+>>>>>>> eaf158766410be3b94702c3c2699f93482407561
 return 0;
 }
